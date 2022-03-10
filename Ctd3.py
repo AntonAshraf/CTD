4db99fc09472c6a1ac636a5f1c3bc2ab7ed7b2a9
Anton Ashraf <51335125+AntonAshraf@users.noreply.github.com>
2022-03-10 03:07:19 +0200

    Update and rename Ctd4.py to Ctd3.py

--- Ctd3.py
+++ Ctd3.py
@@ -1,8 +1,11 @@
 import turtle
 
+turtle.title("My Turtle Program")
 t = turtle.Turtle()
+turtle.bgcolor("black")
 t.color("cyan")
 t.speed(0)
+
 for n in range(1000):
     t.forward(n)
     t.right(50)
