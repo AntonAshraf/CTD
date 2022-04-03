9895dd1a10a13a424a71c47cb25b565393b54639
anton <anton0ashraf@gmail.com>
2022-04-03 04:54:06 +0200

    ctd 4

--- CTD4.py
+++ CTD4.py
@@ -1,13 +1,40 @@
-from cProfile import label
 from tkinter import *
+import hashlib
+import re
 
 root = Tk()
+root.geometry("700x400")
+root.config(bg="light grey")
+root.title("                                                                              Login")
 
-text2 = Label(root, text="let's start")
-text = Label(root, text="hello")
+password = "SuperHardPassword99"
+hashpass = hashlib.new("md5",password.encode("utf-8")).hexdigest()
 
-text2.grid(row=0,column=0)
-text.grid(row=0,column=0)
+def click():
+  user = userInput.get()
+  pas = passInput.get()
+  hpass = hashlib.md5(pas.encode("utf-8")).hexdigest()
+  
+  regex = "[@_!#$%^&*()<>?/\|}{~:]"
+  special = re.compile(regex)
+  
+  if  user == "guest" and hpass == hashpass:
+    Label(root, text="Thats right! Login is sucsseful!", fg="green", font="bold").place(x= 160, y=300)
+  elif user == "" or pas == "":
+    Label(root, text="Enter tht username or the password!", fg="red", font="bold").place(x= 160, y=300)
+  elif special.search(user) != None or special.search(pas):
+    Label (root, text="There is speacial charcther in the Username!", fg="red", font="bold").place(x= 160, y=300)
+  else:
+    Label (root, text="Wrong Username or password", fg="red", font="bold").place(x= 160, y=300)
+
+userInput = Entry(root, font="sans",width=20, fg="blue")
+passInput = Entry(root, font="sans",width=20, fg="blue")
+userInput.place(x=260,y=35)
+passInput.place(x=260, y=105)
+passInput.config(show="•")
+
+username = Label (root, text= "Username: ",font="bold",bg ="light grey").grid(row=0,column=  0, pady=30, padx=150)
+password = Label (root, text= "Password: ",font="bold",bg ="light grey").grid(row=1,column=  0, pady=10, padx= 150)
+button = Button (root, text="Login" ,  padx= 30, pady=10, fg="black",border=1,bg="light grey", command=click).place(x=240,y= 180)
 
 root.mainloop()
-
