78c8ece5d25d514258c45b1c3ec350d0de7a4b39
anton <anton0ashraf@gmail.com>
2022-05-05 03:46:04 +0200

    Update

--- CTD6.py
+++ CTD6.py
@@ -16,7 +16,7 @@ while True:
   if (isTrue):
     if cv2.waitKey(10) & 0xFF == ord('s'):
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)      
-      _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+      _, thresh = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY)
       contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
       for contour in contours:
         if i == 0:
@@ -24,15 +24,19 @@ while True:
           continue
         approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
         # cv2.drawContours(frame, [contour], 0, (0, 255, 0), 1)
-        if len(approx) == 1:      # Line
+        if len(approx) == 1:               # Line
           shapes[2] += 1
-        elif len(approx) == 3:    # Triangle
+        elif len(approx) == 3:             # Triangle
           shapes[1] += 1
-        elif len(approx) == 4:    # Square
-          shapes[3] += 1    
-        else:                     # Circle
-          shapes[0] += 1
-          
+        elif len(approx) == 4:             # Square
+          x, y, w, h = cv2.boundingRect(approx)
+          aspectRatio = float(w) / h
+          if  0.95 <= aspectRatio <= 1.05:
+            shapes[3] += 1                # Square
+          else:
+            shapes[2] += 1                # Line
+        else:
+          shapes[0] += 1                  # Circle
       for j in range (4):
         cv2.putText(output,f'{shapes[j]}', dim[j], cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
       break
